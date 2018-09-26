c5ad791813a6723733c4bda1e7d120f8ff8b8b4d
t3db0t <t3db0t@gmail.com>
2018-09-26 17:31:12 -0400

    testing minor changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="t3db0t | Technology, Design, Invention" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,10 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          // <figure className="image">
+          //   <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          // </figure>
+          t3db0t
         </Link>
       </div>
       <div className="navbar-start">
@@ -20,18 +21,6 @@ const Navbar = () => (
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
